8d9af9150ba83144f077f04af9471e85f0012617
Hemrajsinh Varachhia <hemrajkv793@gmail.com>
2020-05-27 17:00:44 -0700

    contact-list display with btn on contact page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
-//modules
+// Modules
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//components
+
+// Components
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { PagesNotFoundComponent } from './pages/pages-not-found/pages-not-found.component';
-
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 const routes: Routes = [
-  {path: 'home', component:HomeComponent, data:{title: 'Home'}},
-  {path: 'about', component:AboutComponent, data:{title:'About'}},
-  {path:'contact', component:ContactComponent, data:{title: 'Contact'}},
-  {path: 'products', component:ProductsComponent, data:{title: 'Products'}},
-  {path:'services', component:ServicesComponent, data:{title: 'Sevices'}},
+  {path: 'home', component: HomeComponent, data: {title: 'Home'}},
+  {path: 'about', component: AboutComponent, data: {title: 'About'}},
+  {path: 'products', component: ProductsComponent, data: {title: 'Products'}},
+  {path: 'services', component: ServicesComponent, data: {title: 'Services'}},
+  {path: 'contact', component: ContactComponent, data: {title: 'Contact'}},
+  {path: 'contact/contact-list', component: ContactListComponent, data: {title: 'Contact List'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: PagesNotFoundComponent}
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,11 @@
+//modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
+
+//components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
@@ -11,6 +15,13 @@ import { AboutComponent } from './pages/about/about.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { PagesNotFoundComponent } from './pages/pages-not-found/pages-not-found.component';
+
+
+//services
+import {FlashMessagesModule, FlashMessagesService} from 'angular2-flash-messages';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
 
 @NgModule({
   declarations: [
@@ -23,12 +34,17 @@ import { ContactComponent } from './pages/contact/contact.component';
     ProductsComponent,
     ServicesComponent,
     ContactComponent,
+    PagesNotFoundComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [FlashMessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
